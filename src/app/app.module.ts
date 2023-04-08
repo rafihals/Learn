4c8a5fdfb00198492dc0542aa77bf44029fba212
rafihals <ravi.pkl@uii.ac.id>
2023-04-08 14:28:15 +0700

    sidenav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BodyComponent } from './body/body.component';
+import { CoupensComponent } from './coupens/coupens.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MediaComponent } from './media/media.component';
+import { PagesComponent } from './pages/pages.component';
+import { ProductsComponent } from './products/products.component';
+import { SettingsComponent } from './settings/settings.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { StatisticsComponent } from './statistics/statistics.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BodyComponent,
+    CoupensComponent,
+    DashboardComponent,
+    MediaComponent,
+    PagesComponent,
+    ProductsComponent,
+    SettingsComponent,
+    SidenavComponent,
+    StatisticsComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/sidenav/nav-data.ts
@@ -0,0 +1,37 @@
+export const navbarData = [
+  {
+    routerlink: 'dashboard',
+    icon: 'fa fa-home',
+    label: 'Dashboard',
+  },
+  {
+    routerlink: 'products',
+    icon: 'fa fa-box-open',
+    label: 'Products',
+  },
+  {
+    routerlink: 'statistic',
+    icon: 'fa fa-chart-bar',
+    label: 'Statistic',
+  },
+  {
+    routerlink: 'coupens',
+    icon: 'fa fa-tags',
+    label: 'Coupens',
+  },
+  {
+    routerlink: 'pages',
+    icon: 'fa fa-file',
+    label: 'Pages',
+  },
+  {
+    routerlink: 'media',
+    icon: 'fa fa-camera',
+    label: 'Media',
+  },
+  {
+    routerlink: 'settings',
+    icon: 'fa fa-cog',
+    label: 'Settings',
+  },
+];
